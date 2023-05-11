defcf707b00c5c034035a9431c0c7582832b4cc4
yicheng <yichez16@uci.edu>
2023-05-10 23:20:09 -0700

    update

--- examples/kernel.cu
+++ examples/kernel.cu
@@ -56,6 +56,6 @@ __global__ void copyKernel_two(int* local, int* remote, int threadID1, int threa
     // }
     clock_t stopClock = clock();
     clock_t elapsedTime = stopClock - startClock;
-    printf("ThreadID: %d,Elapsed Time: %llu cycles\n", threadID2, elapsedTime);
+    printf("%llu\n", elapsedTime);
 
 }
